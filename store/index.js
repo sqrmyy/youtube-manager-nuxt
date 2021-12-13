18124ba07030bd05d162480d8a4ca9224fad3b8b
sqrmyy <dn41.ra20adtdjw@gmail.com>
2021-12-13 19:49:30 +0900

    fix: 7.2.6 アカウント登録機能を使ってみる

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import { createRequestClient } from '~/store/request-client'
+import firebase from '@/plugins/firebase'
 
 export const state = () => ({
   items: [],
@@ -7,6 +8,7 @@ export const state = () => ({
   item: {},
   meta: {},
   searchMeta: {},
+  token: '',
 })
 
 export const actions = {
@@ -33,6 +35,21 @@ export const actions = {
     const res = await client.get(payload.uri, payload.params)
     commit('mutateSearchVideos', res)
   },
+  async signUp({ commit, dispatch }, payload) {
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(payload.email, payload.password)
+    const res = await firebase
+      .auth()
+      .signInWithEmailAndPassword(payload.email, payload.password)
+    const token = await res.user.getIdToken()
+    this.$cookies.set('jwt_token', token)
+    commit('mutateToken', token)
+    this.app.router.push('/')
+  },
+  setToken({ commit }, payload) {
+    commit('mutateToken', payload)
+  },
 }
 
 export const mutations = {
@@ -53,6 +70,9 @@ export const mutations = {
       ? state.searchItems.concat(payload.items)
       : []
     state.searchMeta = payload
+  },
+  mutateToken(state, payload) {
+    state.token = payload
   },
 }
 
